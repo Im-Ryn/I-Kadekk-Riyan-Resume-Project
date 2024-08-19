3328e69f6dd255f48a6c415846ea1e022901bcf1
Im-Ryn <sayriariyan@gmail.com>
2024-08-19 22:06:47 +0700

    Menyelesaikan pembuatan resume untuk laptop responsive

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'basic-colors-cstm': '#CFCFD1',
+        'blue-custom': '#202E4F'
+      },
+      fontFamily: {
+        Roboto: ['Roboto'],
+      },
+      spacing: {
+        '8.5': '34px',
+        '19' : '78px',
+        '70': '270px',
+        '100': '420px',
+        '50': '202px',
+        '74': '310px',
+      }
+    },
+  },
+  plugins: [],
+}
+
